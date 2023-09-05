71541558db03a48964cd37add28bbb7a9abf2bb2
ccaakk <2452821658@qq.com>
2023-09-05 09:05:15 +0800

    1.0.1

---
+++ imooc-video-api/src/main/java/com/imooc/ImoocVideoApp.java
@@ -0,0 +1,26 @@
+package com.imooc;
+
+import com.imooc.video.service.websocket.WebSocketService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableAsync
+@EnableScheduling
+@EnableFeignClients(basePackages= "com.imooc.video.service.feign")
+@EnableHystrix
+public class ImoocVideoApp {
+
+    public static void main(String[] args){
+        ApplicationContext app = SpringApplication.run(ImoocVideoApp.class, args);
+        WebSocketService.setApplicationContext(app);
+    }
+
+}
